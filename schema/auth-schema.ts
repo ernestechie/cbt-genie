f93f94e26e813656b827bc8746f2f1d446a8698b
Isaiah Ernest Ovie <officialisaiahovie@gmail.com>
2025-03-25 02:55:43 +0100

    migrate from nextjs to honojs api routes

--- schema/auth-schema.ts
+++ schema/auth-schema.ts
@@ -5,15 +5,22 @@ export const emailSchema = z
   .min(1, "Email is required")
   .email("Invalid email format");
 
+export const otpCodeSchema = z
+  .string()
+  .min(6, "OTP must be 6 digits")
+  .max(6, "OTP must be 6 digits");
+
 export const otpFormSchema = z.object({
-  otpCode: z
-    .string()
-    .min(6, "OTP must be 6 digits")
-    .max(6, "OTP must be 6 digits"),
+  otpCode: otpCodeSchema,
 });
 
 export const emailFormSchema = z.object({
   email: emailSchema,
+});
+
+export const verificationFormSchema = z.object({
+  email: emailSchema,
+  otpCode: otpCodeSchema,
 });
 
 export type EmailFormType = z.infer<typeof emailFormSchema>;
